98459bd3cbeb630ca9043b08317215a0c38c4d3d
Sofiane Beors <theabstractdev@gmail.com>
2016-05-11 16:59:01 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -6,10 +6,20 @@ then
 	then
 		if [ $1 = "production" ]
 		then
-		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
+      if [ $3 ]
+      then
+        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
+      else
+		   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" -e "IP=$3" scheduly
+      fi
 		elif [ $1 = "developpement" ]
 		then
-		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" node:onbuild
+      if [ $3 ]
+      then
+		   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" -e "IP=$3" node:onbuild
+      else
+        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" node:onbuild
+      fi
 		fi
 	else
 		echo "Missing MONGO_URL"

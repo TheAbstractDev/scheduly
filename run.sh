ef34273db569e2799ad2b3ac677d49f1285c17a7
Sofiane Beors <theabstractdev@gmail.com>
2016-05-11 17:32:51 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,24 +6,26 @@ then
 	then
 		if [ $1 = "production" ]
 		then
-      if [ $3 ]
-      then
-        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
-      else
-		   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" -e "IP=$3" scheduly
-      fi
+	      if [ $3 ]
+	      then
+	        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
+	      else
+			   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" -e "IP=$3" scheduly
+	      fi
 		elif [ $1 = "developpement" ]
 		then
-      if [ $3 ]
-      then
-		   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" -e "IP=$3" node:onbuild
-      else
-        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" node:onbuild
-      fi
+	      if [ $3 ]
+	      then
+			   	docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" -e "IP=$3" node:onbuild
+	      else
+	        docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" node:onbuild
+	      fi
+	    else
+			echo "Wrong value"
 		fi
 	else
-		echo "Missing MONGO_URL"
+		echo "Error: Wrong value"
 	fi
 else
-	echo "Missing environement variables"
+	echo "Error: Missing environement variables"
 fi

f9b3bdb1154c401a9a0e186539ace85724af8886
Sofiane Beors <theabstractdev@gmail.com>
2016-05-11 17:41:09 +0200

    Remove build.sh & run.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker build -t scheduly .
-docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app scheduly npm install
-docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app scheduly ./minify.sh

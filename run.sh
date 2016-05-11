f067deb9d1d718005e9f8b6b2819dc1ce6f34e07
Sofiane Beors <theabstractdev@gmail.com>
2016-05-11 16:53:50 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ then
 	then
 		if [ $1 = "production" ]
 		then
-		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net="host" -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
+		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "NODE_ENV=production" -e "MONGO_URL=$2" scheduly
 		elif [ $1 = "developpement" ]
 		then
-		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net="host" -e "MONGO_URL=$2" node:onbuild
+		   		docker run --rm -ti -p 8080:8080 -v "$PWD":/usr/src/app --net host -e "MONGO_URL=$2" node:onbuild
 		fi
 	else
 		echo "Missing MONGO_URL"
